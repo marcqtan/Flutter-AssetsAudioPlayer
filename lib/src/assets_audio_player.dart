7b387103ba1841764af261227509a2c9c54f197f
Marc Tan <marc.tan@fullspeedtechnologies.com>
2021-05-19 14:46:25 +0800

    Retain playspeed

--- lib/src/assets_audio_player.dart
+++ lib/src/assets_audio_player.dart
@@ -1027,7 +1027,7 @@ class AssetsAudioPlayer {
 
         await setLoopMode(loopMode);
 
-        await setPlaySpeed(playSpeed);
+        await setPlaySpeed(this.playSpeed.value);
 
         _stopped = false;
         _playlistFinished.value = false;
